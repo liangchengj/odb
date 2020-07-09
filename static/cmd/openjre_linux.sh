4b3d79fc8339b2503a06c57b94a5dcc60b2c8a7e
liangchengj <liangchengj@outlook.com>
2020-07-09 20:35:28 +0800

    2020/7/9 20:35

---
+++ static/cmd/openjre_linux.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+# Created at 2020/7/9 19:44.
+# @author Liangcheng Juves
+
+xz_filename=linux.tar.xz
+jdk_from_http=https://odb.liangchengj.com/static/xz/openjre/11.0.7+10/$xz_filename
+jdk_install_path=/usr/local/openjre/11.0.7+10/
+
+sudo wget -P /usr/local/ $jdk_from_http
+sudo mkdir /usr/local/openjre
+sudo mkdir $jdk_install_path
+sudo tar -xvJf /usr/local/$xz_filename -C $jdk_install_path
+sudo rm -rf /usr/local/$xz_filename
+
+# Env 
+echo "export JAVA_HOME=\"$jdk_install_path\"
+export CLASSPATH=.:\$JAVA_HOME/lib
+export PATH=\$JAVA_HOME/bin:\$PATH" >> /etc/profile
+
+source /etc/profile
+
+echo "
+Complete!
+"
+java --version
+
+echo "
+"
