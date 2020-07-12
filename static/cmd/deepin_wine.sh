6451c5801651bfe49d11d85b82a53ab7bac7ec4b
Liangcheng Juves <liangchengj@outlook.com>
2020-07-12 08:01:16 -0700

    Update deepin_wine.sh

--- static/cmd/deepin_wine.sh
+++ static/cmd/deepin_wine.sh
@@ -18,9 +18,9 @@ url_begin=https://mirrors.aliyun.com/deepin/pool
 
 
 install_wine_env_deb=(
-        non-free/d/deepin-wine/deepin-wine_2.18-22~rc0_all.deb
-        non-free/d/deepin-wine/deepin-wine32_2.18-22~rc0_i386.deb
-        non-free/d/deepin-wine/deepin-wine32-preloader_2.18-22~rc0_i386.deb
+        non-free/d/deepin-wine/deepin-wine_2.18-20_all.deb
+        non-free/d/deepin-wine/deepin-wine32_2.18-20_i386.deb
+        non-free/d/deepin-wine/deepin-wine32-preloader_2.18-20_i386.deb
         non-free/d/deepin-wine-helper/deepin-wine-helper_1.2deepin8_i386.deb
         # non-free/d/deepin-wine-helper64/deepin-wine-helper64_1.1-0_amd64.deb
         non-free/d/deepin-wine-plugin/deepin-wine-plugin_1.0deepin2_amd64.deb
@@ -28,13 +28,13 @@ install_wine_env_deb=(
 	non-free/d/deepin-wine-plugin-virtual/deepin-wine-plugin-virtual_1.0deepin3_all.deb
 	non-free/d/deepin-wine-uninstaller/deepin-wine-uninstaller_0.1deepin2_i386.deb
 	non-free/u/udis86/udis86_1.72-2_i386.deb
-	non-free/d/deepin-wine/deepin-fonts-wine_2.18-22~rc0_all.deb
-	non-free/d/deepin-wine/deepin-libwine_2.18-22~rc0_i386.deb
-	main/libj/libjpeg-turbo/libjpeg-turbo-progs_1.5.1-2_amd64.deb
-	main/libj/libjpeg-turbo/libjpeg-turbo-progs_1.5.1-2_i386.deb
-	non-free/d/deepin-wine/deepin-libwine-dbg_2.18-22~rc0_i386.deb
-	non-free/d/deepin-wine/deepin-libwine-dev_2.18-22~rc0_i386.deb
-	non-free/d/deepin-wine/deepin-wine-binfmt_2.18-22~rc0_all.deb
+	non-free/d/deepin-wine/deepin-fonts-wine_2.18-20_all.deb
+	non-free/d/deepin-wine/deepin-libwine_2.18-20_i386.deb
+	main/libj/libjpeg-turbo/libjpeg62-turbo_1.5.1-2_amd64.deb
+	main/libj/libjpeg-turbo/libjpeg62-turbo_1.5.1-2_i386.deb
+	non-free/d/deepin-wine/deepin-libwine-dbg_2.18-20_i386.deb
+	non-free/d/deepin-wine/deepin-libwine-dev_2.18-20_i386.deb
+	non-free/d/deepin-wine/deepin-wine-binfmt_2.18-20_all.deb
 )
 
 
