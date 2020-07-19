b3c615522efc1468ac5685716301b80f0ce71062
liangchengj <liangchengj@outlook.com>
2020-07-19 10:43:10 +0800

    2020/7/19 10:43

--- static/cmd/msvc_env.bat
+++ static/cmd/msvc_env.bat
@@ -4,5 +4,6 @@
 
 setx /m INCLUDE "C:\Program Files (x86)\Microsoft Visual Studio\2019\BuildTools\VC\Tools\MSVC\14.26.28801\include;C:\Program Files (x86)\Windows Kits\10\Include\10.0.18362.0\ucrt"
 setx /m LIB "C:\Program Files (x86)\Microsoft Visual Studio\2019\BuildTools\VC\Tools\MSVC\14.26.28801\lib\onecore\x64;C:\Program Files (x86)\Microsoft Visual Studio\2019\BuildTools\VC\Tools\MSVC\14.26.28801\lib\x64;C:\Program Files (x86)\Windows Kits\10\Lib\10.0.18362.0\ucrt\x64;C:\Program Files (x86)\Windows Kits\10\Lib\10.0.18362.0\um\x64"
-echo "The environment variable is set successfully."
+
+echo The environment variable is set successfully.
 pause
